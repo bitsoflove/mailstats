53d48893b7261d044c5e46c29b2662028f6d96e1
Stijn Tilleman <stijn@bitsoflove.be>
2016-03-04 16:42:52 +0100

    Added category check

--- tests/LogResponseTest.php
+++ tests/LogResponseTest.php
@@ -6,12 +6,15 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class LogResponseTest extends TestCase
 {
+    use DatabaseTransactions;
 
     protected $mailStatisticClass;
     protected $mailStatistics;
     protected $logResponseClass;
     protected $projectClass;
     protected $projects;
+    protected $categoryClass;
+    protected $categories;
 
     public function setUp()
     {
@@ -23,7 +26,31 @@ class LogResponseTest extends TestCase
         $this->projectClass = \BitsOfLove\MailStats\Entities\Project::class;
         $this->projects = $this->app->make($this->projectClass);
 
+        $this->categoryClass = \BitsOfLove\MailStats\Entities\Category::class;
+        $this->categories = $this->app->make($this->categoryClass);
+
         $this->logResponseClass = \BitsOfLove\MailStats\LogResponse::class;
+    }
+
+    protected function createMailStatistic()
+    {
+        $project = $this->createProject();
+        $category = $this->createCategory();
+
+        $data = [
+            'recipient' => "to@email.com",
+            'tag' => 'tag',
+            'status' => 'queued', // default state
+            'project_id' => $project->id,
+            'category_id' => $category->id,
+            'service_message_id' => "testmessageid",
+        ];
+
+        return [
+            $this->mailStatistics->create($data),
+            $project,
+            $category
+        ];
     }
 
     protected function createProject()
@@ -36,16 +63,26 @@ class LogResponseTest extends TestCase
         return $this->projects->create($data);
     }
 
+    protected function createCategory()
+    {
+        $data = [
+            'name' => 'mail'
+        ];
+
+        return $this->categories->create($data);
+    }
+
     /** @test */
     public function logresponse_returns_correct_logresponse_instance()
     {
         $project = $this->createProject();
+        $this->createCategory();
 
         $data = [
             'recipient' => 'some@email.to',
             'tag' => $project->name,
             'status' => "delivered",
-            'service_id' => 'insertserviceidhere'
+            'message-id' => 'insertserviceidhere'
         ];
 
         $request = new \Illuminate\Http\Request([], $data);
@@ -60,6 +97,8 @@ class LogResponseTest extends TestCase
      */
     public function logresponse_with_non_existing_project_in_tag_throws_exception()
     {
+        $this->createCategory();
+
         $data = [
             'recipient' => 'some@email.to',
             'tag' => "testing",
@@ -69,5 +108,24 @@ class LogResponseTest extends TestCase
 
         $request = new \Illuminate\Http\Request([], $data);
         \BitsOfLove\MailStats\LogResponse::create($request);
+    }
+
+    /** @test */
+    public function logresponse_finds_correct_category_for_given_service_message_id()
+    {
+        list($mailStatistic, $project, $category) = $this->createMailStatistic();
+
+        $data = [
+            'recipient' => 'some@email.to',
+            'tag' => $project->name,
+            'status' => "delivered",
+            'message-id' => $mailStatistic->service_message_id,
+        ];
+
+        $request = new \Illuminate\Http\Request([], $data);
+        $logResponse = \BitsOfLove\MailStats\LogResponse::create($request);
+
+        $this->assertInstanceOf($this->logResponseClass, $logResponse);
+        $this->assertInstanceOf($this->categoryClass, $logResponse->getCategory());
     }
 }
